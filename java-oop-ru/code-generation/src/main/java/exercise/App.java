7c2d413a240e21fa94da6b9b8b6ea03859bae0ca
@hexlet/cli <support@hexlet.io>
2024-09-10 23:51:14 +0200

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -6,26 +6,16 @@ import java.nio.file.StandardOpenOption;
 
 // BEGIN
 public class App {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static void save(Path path, Car car) {
-        try {
-            // Сериализация объекта Car в JSON и запись в файл
-            objectMapper.writeValue(path.toFile(), car);
-        } catch (IOException e) {
-            e.printStackTrace(); // Обработка исключений
-        }
+    public static void save(Path filePath, Car instance) throws Exception {
+        String jsonRepresentation = instance.serialize();
+        Files.writeString(filePath, jsonRepresentation, StandardOpenOption.WRITE);
     }
 
-    public static Car extract(Path path) {
-        try {
-            // Чтение JSON из файла и десериализация в объект Car
-            return objectMapper.readValue(path.toFile(), Car.class);
-        } catch (IOException e) {
-            e.printStackTrace(); // Обработка исключений
-            return null; // В случае ошибки вернуть null
-        }
+    public static Car extract(Path filePath) throws Exception {
+        String jsonRepresentation = Files.readString(filePath);
+        Car instance = Car.deserialize(jsonRepresentation);
+        return instance;
     }
 }
-
 // END
--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -14,27 +14,15 @@ class Car {
     User owner;
 
     // BEGIN
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public String serialize() {
-        try {
-            return objectMapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null; // или выбросить исключение
-        }
+    public String serialize() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRepresentation = objectMapper.writeValueAsString(this);
+        return jsonRepresentation;
     }
 
-    public static Car deserialize(String json) {
-        try {
-            return objectMapper.readValue(json, Car.class);
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-            return null; // или выбросить исключение
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null; // или выбросить исключение
-        }
+    public static Car deserialize(String jsonRepresentation) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonRepresentation, Car.class);
     }
     // END
 }
